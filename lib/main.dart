dd28ebd7dae76b067c2978db2e35d1a2d4fd5d52
jaafar <jaafar@cnepho.com>
2023-01-28 10:48:57 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BookAiApp());
+}
+
+class BookAiApp extends StatelessWidget {
+  const BookAiApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: Container(),
+    );
+  }
+}
